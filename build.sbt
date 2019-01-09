2d416e019e0f16f392f0e2420dc50f1ef7f68232
mfgm2 <repom2@airmail.cc>
2019-01-09 14:53:09 +0300

    Pattern parser test

--- build.sbt
+++ build.sbt
@@ -8,5 +8,6 @@ test in assembly := {}
 
 libraryDependencies ++= Seq(
   "com.github.scopt" %% "scopt" % "4.0.0-RC2",
-  "net.sf.extjwnl" % "extjwnl" % "2.0.0"
+  "net.sf.extjwnl" % "extjwnl" % "2.0.0",
+  "org.scalatest" %% "scalatest" % "3.0.5" % Test
 )
---
+++ src/test/scala/patnmgen/UtilTest.scala
@@ -0,0 +1,37 @@
+package patnmgen
+
+import org.scalatest.{FlatSpec, Matchers}
+import Utils._
+
+class UtilTest extends FlatSpec with Matchers {
+
+  behavior of "Pattern parser"
+
+  it should "parse patterns and apply token-replaces" in {
+
+    val mapper: ((Int, String)) => String = {
+      case (i,t) => s"|$i$t|"
+    }
+
+    val bounds = ("<", ">")
+
+    val inp = List(
+      "foo<A>bar<B>baz" -> "foo|0A|bar|1B|baz",
+      "<A>foobar" -> "|0A|foobar",
+      "<E>-<A>--<C>" -> "|0E|-|1A|--|2C|",
+      "foobar<FOO>" -> "foobar|0FOO|",
+      "aaa<FOO>bbb<BARR>" -> "aaa|0FOO|bbb|1BARR|"
+    )
+
+    inp.foreach {
+      case (pat, ref) =>
+        println(pat)
+        val parsed = patternParse(pat, bounds)
+        val mapped = parsed.tokens.map(mapper)
+        val resp = makePatternList(parsed, mapped)
+        resp shouldBe ref
+    }
+
+  }
+
+}
